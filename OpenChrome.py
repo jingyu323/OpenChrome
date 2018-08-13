c0f376d605ae7ec586bba7d3521209badf938715
Rain <Rain@RaindeMacBook-Pro.local>
2018-08-13 23:55:06 +0800

    添加注释

--- OpenChrome.py
+++ OpenChrome.py
@@ -2,7 +2,10 @@ from selenium import webdriver
 
 from selenium import webdriver
 
+##打开chrome
+
 driver = webdriver.Chrome('/usr/local/bin/chromedriver');
 driver.get('http://www.baidu.com')
 driver.find_element_by_id('kw').send_keys('Selenium')
+#模拟点击事件
 driver.find_element_by_id('su').click()
